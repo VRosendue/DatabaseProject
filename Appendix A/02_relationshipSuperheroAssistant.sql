b5be1c11a531d6b193d8de92f7140fdb5b5b650a
VRosendue <110988416+VRosendue@users.noreply.github.com>
2022-08-25 21:14:24 +0200

    Update 02_relationshipSuperheroAssistant.sql
    14 and 16 should work, need testing with data tho

--- Appendix A/02_relationshipSuperheroAssistant.sql
+++ Appendix A/02_relationshipSuperheroAssistant.sql
@@ -8,3 +8,9 @@ ALTER TABLE power ADD COLUMN Superhero_id int REFERENCES superhero;
 ALTER TABLE assistant ADD COLUMN Superhero_id int REFERENCES superhero
 
 ALTER TABLE assistant ADD CONSTRAINT fk_superhero FOREIGN KEY(superhero_id) REFERENCES superhero(id) ON DELETE CASCADE;
+
+
+
+ALTER TABLE assistant ADD COLUMN Superhero_id int REFERENCES superhero;
+
+ALTER TABLE assistant ADD CONSTRAINT fk_superhero FOREIGN KEY (Superhero_id) REFERENCES superhero(Superhero_id) ON DELETE CASCADE;
