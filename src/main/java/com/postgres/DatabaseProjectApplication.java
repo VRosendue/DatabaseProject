cc3b418ccb4760822157b5a25e3bb5746833959d
VRosendue <110988416+VRosendue@users.noreply.github.com>
2022-08-25 12:30:00 +0200

    Update DatabaseProjectApplication.java
    Updated with offset and limit method - Has been tested, and works properly

--- src/main/java/com/postgres/DatabaseProjectApplication.java
+++ src/main/java/com/postgres/DatabaseProjectApplication.java
@@ -17,6 +17,7 @@ public class DatabaseProjectApplication {
 		
 		//GetActors.updateCustomer("Mathias", "Gylden", "Denmark", "3670", "64523", "Math.Gyld@net", 60); - Appendix B opgave 6 
                 //GetActors.createCustomer(60, "Testerson", "Testthis", "Denmark", "3670", "279384", "test@test"); - Appendix B opgave 5
+		//GetActors.limitCustomerSearch(5, 2); - Appendix B opgave 4 - Limit, offset
 		//GetActors.findCustomerByName("Enrique"); //Appendix B opgave 3 - VIRKER NOGENLUNDE KUNNE BRUGE ET FIX
 		//GetActors.findCustomerById(50); //Appendix B opgave 2 - Read specific customer by ID
 		
